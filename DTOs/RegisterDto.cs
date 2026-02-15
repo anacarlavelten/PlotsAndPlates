e2d1d32d264f7d154cc2354bec5dc79235085b4b
Ana Velten <naniacarla324@gmail.com>
2026-02-14 23:05:24 -0300

    Adicionando DTO

---
+++ DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+namespace PlotsAndPlates.Backend.DTOs
+{
+    public class RegisterDto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+    }
+}
